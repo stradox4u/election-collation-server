a44958a59d5de7202678ea34437803851d72c522
Umar Odeh Adejoh <stradox@gmail.com>
2023-03-19 13:33:17 +0100

    moved port for development into an environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(8000);
+  await app.listen(port || 80);
 }
 bootstrap();
